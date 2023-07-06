24c12d78269e365f7d273c2bf1f0c660f90ce3a3
MaksimLisovoi <lisovoi123@gmail.com>
2023-07-06 18:20:04 +0300

    +

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ const darkTheme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <App />
